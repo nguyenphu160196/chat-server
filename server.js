8b43d84550dbdfb43b3fa7999180776128dc01eb
ThienPhu <nguyenphu160196@gmail.com>
2018-03-14 23:37:04 +0700

    update

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.use('/', index);
 app.use('/api/', user);
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
   next();
