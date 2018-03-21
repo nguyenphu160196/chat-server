949da168e436f5a44536dd184f2842b2f23bc697
ThienPhu <nguyenphu160196@gmail.com>
2018-03-21 21:16:31 +0700

    upload project

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: 'khoaluantotnghiep',
-    database: 'mongodb://kltn_2018:kltn!!!@ds012538.mlab.com:12538/kltn_db'
-    // database: 'mongodb://localhost:27017/kltn_db'  
+    // database: 'mongodb://kltn_2018:kltn!!!@ds012538.mlab.com:12538/kltn_db'
+    database: 'mongodb://localhost:27017/kltn_db'  
   }
